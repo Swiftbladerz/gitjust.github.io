52620817ce6547571fa732a5b0961f0e8a520baf
Swiftbladerz <64571273+Swiftbladerz@users.noreply.github.com>
2020-06-15 00:44:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,19 +5,14 @@ self.addEventListener('install', function(event) {
         cache.addAll([
           '/',
           '/index.html',
-          '/manifest.json',
           '/src/css/app.css',
-          '/src/js/app.js',
-          '/src/icons/icon-96x96.png',
-          '/src/icons/icon-144x144.png',
-          '/src/icons/icon-192x192.png',
-          '/src/icons/icon-384x384.png',
-          '/src/icons/icon-512x512.png'
+          '/src/js/app.js'
         ])
       })
   );
   return self.clients.claim();
 });
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
